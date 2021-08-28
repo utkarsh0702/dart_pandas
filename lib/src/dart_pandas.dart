ff8a37682b6c7a3b427f243682ac02a7be9addf0
utkarsh0702 <utkarsh.um07@gmail.com>
2021-08-28 23:21:18 +0530

    read_csv update

--- lib/src/dart_pandas.dart
+++ lib/src/dart_pandas.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:csv/csv.dart';
 
 class DartPandas {
   const DartPandas();
@@ -9,27 +10,62 @@ class DartPandas {
   Future<List> read_csv(String address, {List? columns}) async {
     List data = [], columnsNames = [];
     final csvFile = new File(address).openRead();
+    print('CSV File read');
     final contents = await csvFile
         .transform(utf8.decoder)
-        .transform(LineSplitter())
+        .transform(new CsvToListConverter())
         .toList();
-    if (columns == null) {
-      columnsNames = contents[0].split(',');
-    } else {
-      columnsNames = columns;
-    }
-    contents.forEach((element) => data.add(element.split(',')));
-    List columnData = List.generate(columnsNames.length,
-        (i) => List.filled(contents.length, null, growable: false),
-        growable: false);
-    for (int i = 0; i < columnsNames.length; i++) {
-      for (int j = 0; j < contents.length; j++) {
-        columnData[i][j] = data[j][i];
+    print('Contents extracted');
+    print(contents);
+    int row = 0, count = 0;
+    while (true) {
+      var rowData = [];
+      for (int i = count; i < contents[0].length; i++) {
+        try {
+          if (contents[0][i].contains('\n')) {
+            data.add(rowData);
+            break;
+          } else {
+            rowData.add(contents[0][i]);
+            count += 1;
+          }
+        } catch (_) {}
+      }
+      if (count == contents[0].length) {
+        break;
       }
     }
-    print(columnData);
+    print("Data: $data");
+    // if (columns == null) {
+    //   columnsNames = contents[0].split(',');
+    // } else {
+    //   columnsNames = columns;
+    // }
+    // print('Column Name if else');
+    // contents.forEach((element) => data.add(element.split(',')));
+    // print('data added to another list');
+    // List columnData = List.generate(contents.length,
+    //     (i) => List.filled(columnsNames.length, '', growable: false),
+    //     growable: false);
+    // print('columnData List created');
+    // print(contents);
+    // for (int i = 0; i < columnsNames.length; i++) {
+    //   for (int j = 0; j < contents.length; j++) {
+    //     try {
+    //       if (data[j][i] == null) {
+    //         columnData[i][j] = "";
+    //       } else {
+    //         columnData[i][j] = data[j][i];
+    //       }
+    //     } catch (e) {
+    //       print('Error value: ${data[j][i]}, Error: $e');
+    //     }
+    //   }
+    // }
+    // print('data inserted in columnData.');
+    // print(columnData);
     // var row = contents.length, col = columns_names.length;
 
-    return columnData;
+    return contents;
   }
 }
